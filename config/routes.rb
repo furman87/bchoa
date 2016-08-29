3298e61ce9a8e6798a95d9672d94c8b0f6bda75b
Ken Watson <furman87@gmail.com>
2016-08-29 08:46:53 -0400

    Removed users/sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,10 @@ Rails.application.routes.draw do
   get 'newsletters', to: 'articles#newsletters', as: 'newsletters'
   resources :articles
   resources :board_members
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 end
