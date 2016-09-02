db4be69a00ab0027e82e6f718c580c3f9a7d4374
Ken Watson <furman87@gmail.com>
2016-09-01 20:42:42 -0400

    Disabled /articles/index and /articles/show

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'architectural', to: 'articles#acc', as: 'acc'
   get 'minutes', to: 'articles#minutes', as: 'minutes'
   get 'newsletters', to: 'articles#newsletters', as: 'newsletters'
-  resources :articles
+  resources :articles, except: [:index, :show]
   resources :board_members
   # devise_for :users
   devise_for :users, :skip => [:registrations]
