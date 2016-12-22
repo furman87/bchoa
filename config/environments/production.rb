99f3517377a70b038da63168e3a0ea064b5530fa
Ken Watson <furman87@gmail.com>
2016-12-22 18:55:57 -0500

    Added mail config for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,21 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = {
+    host: 'butlercreek.org'
+  }
+  config.action_mailer.delivery_method = :smtp;
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mail.yahoo.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['mail_username'],
+    password: ENV['mail_password']
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
