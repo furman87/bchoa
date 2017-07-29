c23a372f41e2721a3450455b18d5b7338b5442e9
Ken Watson <furman87@gmail.com>
2017-07-29 13:39:11 -0400

    Mail groups working.

--- app/controllers/admin/mail_groups_controller.rb
+++ app/controllers/admin/mail_groups_controller.rb
@@ -3,13 +3,28 @@ class Admin::MailGroupsController < ApplicationController
   # before_action :authenticate_user!
   
   def index
+    authorize_action_for(MailGroup)
     @mail_groups = MailGroup.order(:name)
   end
   
   def new
+    @mail_group = MailGroup.new
+    authorize_action_for(@mail_group)
+  end
+
+  def create
+    @mail_group = MailGroup.new(mail_group_params)
+    authorize_action_for(@mail_group)
+    if @mail_group.save
+      redirect_to edit_admin_mail_group_path(@mail_group), notice: 'Mail group was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
+    authorize_action_for(@mail_group)
+
     @members = User
       .where("users.email not like '%#{ENV['ignore_listing_domain']}%'")
       .where("users.id in (?)", @mail_group.users.map {|u| u.id})
@@ -19,10 +34,26 @@ class Admin::MailGroupsController < ApplicationController
 
     @non_members = User
       .where("users.email not like '%#{ENV['ignore_listing_domain']}%'")
-      .where("users.id not in (?)", MailGroup.joins(:users).includes(:users).pluck("users.id"))
+      .where("users.id not in (?)", @mail_group.users.any? ? @mail_group.users.map {|u| u.id} : [0])
       .with_residences.order(:last_name).order(:first_name)
       .merge(ResidenceUser.with_residence.only_residents.by_display_order)
       .merge(Residence.with_street)
+  end
+
+  def update
+    authorize_action_for(@mail_group)
+    if @mail_group.update(mail_group_params)
+      redirect_to admin_mail_groups_path, notice: 'Mail group was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize_action_for(@mail_group)
+    @mail_group.users.delete(User.find(@mail_group.users.map {|u| u.id}))
+    @mail_group.destroy
+    redirect_to admin_mail_groups_path, notice: 'Mail group was successfully deleted.'
   end
 
   def add_members
--- app/models/mail_group.rb
+++ app/models/mail_group.rb
@@ -1,4 +1,5 @@
 class MailGroup < ActiveRecord::Base
+  include Authority::Abilities
   has_many :mail_group_members
   has_many :users, through: :mail_group_members
 end
