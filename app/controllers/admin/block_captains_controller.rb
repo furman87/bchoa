ea4517b22bfb1dbf7eb91fdad19f42fad70e3379
Ken Watson <furman87@gmail.com>
2017-08-01 09:56:13 -0400

    Full editing of block captains.

--- app/controllers/admin/block_captains_controller.rb
+++ app/controllers/admin/block_captains_controller.rb
@@ -4,15 +4,28 @@ class Admin::BlockCaptainsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    authorize_action_for(BlockCaptain)
     @block_captains = BlockCaptain.includes(:user).order(:display_order)
   end
 
   def new
     @block_captain = BlockCaptain.new
+    authorize_action_for(@block_captain)
   end
 
   def edit
     authorize_action_for(@block_captain)
+  end
+
+  def create
+    @block_captain = BlockCaptain.new(block_captain_params)
+    authorize_action_for(@block_captain)
+
+    if @block_captain.save
+      redirect_to admin_block_captains_path, notice: 'Block captain was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
@@ -22,6 +35,12 @@ class Admin::BlockCaptainsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    authorize_action_for(@block_captain)
+    @block_captain.destroy
+    redirect_to admin_block_captains_path, notice: 'Block captain was successfully deleted.'
   end
 
   private
@@ -34,7 +53,7 @@ class Admin::BlockCaptainsController < ApplicationController
     @users = User.order(:last_name, :first_name)
   end
 
-# Never trust parameters from the scary internet, only allow the white list through.
+  # Never trust parameters from the scary internet, only allow the white list through.
   def block_captain_params
     params.require(:block_captain).permit(:description, :user_id, :display_order)
   end
